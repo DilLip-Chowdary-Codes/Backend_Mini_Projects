75c3a84e14f4e92b83c9b0c6f9710bfd424108af
DilLip❤️ Kumar Rayapati <CoderDil.DilLip@gmail.com>
2020-07-02 04:20:52 +0000

    Minor changes

--- project_management_portal/adapters/user_service.py
+++ project_management_portal/adapters/user_service.py
@@ -12,17 +12,6 @@ class UserService:
     def is_user_admin(self, user_id: int):
         return self.interface.is_user_admin(user_id)
 
-    def get_user_dto(self, user_id: int):
-        user_dto = self.interface.get_user_dto(user_id)
-        converted_user_dto = UserDto(
-            user_id=user_dto.user_id,
-            username=user_dto.username,
-            profile_pic=user_dto.profile_pic,
-            phone_no=user_dto.phone_no,
-            is_admin=user_dto.is_admin
-            )
-        return converted_user_dto
-
     def get_user_dtos(self, user_ids: List[int]):
 
         user_dtos = self.interface.get_user_dtos(user_ids)
@@ -38,3 +27,14 @@ class UserService:
             ]
 
         return converted_user_dtos
+
+    def get_user_dto(self, user_id: int):
+        user_dto = self.interface.get_user_dtos([user_id])[0]
+        converted_user_dto = UserDto(
+            user_id=user_dto.user_id,
+            username=user_dto.username,
+            profile_pic=user_dto.profile_pic,
+            phone_no=user_dto.phone_no,
+            is_admin=user_dto.is_admin
+            )
+        return converted_user_dto
